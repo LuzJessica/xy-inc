41c2ecc038630e85097bac3744efc141c3363440
Jéssica Luz <jessicaluz@gec.inatel.br>
2019-04-02 15:51:26 -0300

    Ajustes de metodo listar por proximidade

--- src/br/com/xyinc/dao/PontoDAO.java
+++ src/br/com/xyinc/dao/PontoDAO.java
@@ -22,8 +22,8 @@ public class PontoDAO {
 		PreparedStatement ps = conexao.prepareStatement(sql.toString());
 
 		ps.setString(1, ponto.getNome_ponto());
-		ps.setString(2, ponto.getCo_X());
-		ps.setString(3, ponto.getCo_Y());
+		ps.setLong(2, ponto.getCo_X());
+		ps.setLong(3, ponto.getCo_Y());
 		ps.executeUpdate();
 	}
 
@@ -38,75 +38,79 @@ public class PontoDAO {
 		ps.setLong(1, ponto.getIdPonto());
 		ps.executeUpdate();
 	}
-	
-	public void atualizar (Pontos ponto) throws SQLException {
+
+	public void atualizar(Pontos ponto) throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("UPDATE pontos_de_interesse ");
 		sql.append("SET nome_ponto = ?, co_x = ?, co_y = ?");
 		sql.append("WHERE idPonto = ?");
-		
+
 		Connection conexao = ConexaoFactory.conectar();
-		
+
 		PreparedStatement ps = conexao.prepareStatement(sql.toString());
 		ps.setString(1, ponto.getNome_ponto());
-		ps.setString(2, ponto.getCo_X());
-		ps.setString(3, ponto.getCo_Y());
+		ps.setLong(2, ponto.getCo_X());
+		ps.setLong(3, ponto.getCo_Y());
 		ps.setLong(4, ponto.getIdPonto());
 		ps.executeUpdate();
 	}
-	
+
 	public ArrayList<Pontos> listar() throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT * ");
 		sql.append("FROM pontos_de_interesse ");
-		sql.append("ORDER BY nome_ponto ASC");
-		
+		sql.append("ORDER BY idPonto ASC");
+
 		Connection conexao = ConexaoFactory.conectar();
-		
+
 		PreparedStatement ps = conexao.prepareStatement(sql.toString());
-		
+
 		ResultSet rs = ps.executeQuery();
-		
+
 		ArrayList<Pontos> lista = new ArrayList<Pontos>();
-		
-		while(rs.next()) {
+
+		while (rs.next()) {
 			Pontos p = new Pontos();
 			p.setIdPonto(rs.getLong("idPonto"));
 			p.setNome_ponto(rs.getString("nome_ponto"));
-			p.setCo_X(rs.getString("co_x"));
-			p.setCo_Y(rs.getString("co_y"));
-			
+			p.setCo_X(rs.getLong("co_x"));
+			p.setCo_Y(rs.getLong("co_y"));
+
 			lista.add(p);
 		}
 		return lista;
-		
+
 	}
-	
-	public ArrayList<Pontos> listarPorProximidade(Pontos ponto) throws SQLException{
+
+	public ArrayList<Pontos> listarPorProximidade(Pontos ponto) throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT idPonto, nome_ponto, co_x, co_y ");
 		sql.append("FROM pontos_de_interesse ");
-		sql.append("WHERE co_x LIKE ?" );
-		
+
 		Connection conexao = ConexaoFactory.conectar();
-		
+
 		PreparedStatement ps = conexao.prepareStatement(sql.toString());
-		ps.setString(1, ponto.getCo_X());
-		
+
 		ResultSet rs = ps.executeQuery();
 		ArrayList<Pontos> lista = new ArrayList<Pontos>();
-		
-		while(rs.next()) {
+
+		while (rs.next()) {
 			Pontos p = new Pontos();
 			p.setIdPonto(rs.getLong("idPonto"));
 			p.setNome_ponto(rs.getString("nome_ponto"));
-			p.setCo_X(rs.getString("co_x"));
-			p.setCo_Y(rs.getString("co_y"));
-			
-			lista.add(p);
+			p.setCo_X(rs.getLong("co_x"));
+			p.setCo_Y(rs.getLong("co_y"));
+			p.setCo_X_ref(ponto.getCo_X_ref());
+			p.setCo_Y_ref(ponto.getCo_Y_ref());
+			p.setdMax(ponto.getdMax());
+
+			if (p.getCo_X() <= p.getCo_X_ref() + p.getdMax() && p.getCo_Y() <= p.getCo_Y_ref() + p.getdMax()) {
+
+				lista.add(p);
+			}
 		}
 		return lista;
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -138,55 +142,40 @@ public class PontoDAO {
 		 * } // TODO: handle exception }
 		 * 
 		 */
-		
-		/* TESTE ATUALIZAÇÃO
-		
-		p.setIdPonto(1L);
-		p.setNome_ponto("Posto");
-		p.setCo_X("31");
-		p.setCo_Y("18");
-		
-		try{
-			pDAO.atualizar(p);
-			System.out.println("Ponto atualizado com sucesso!");
-			
-		}catch(SQLException e) {
-			System.out.println("Ponto não pode ser atualizado!");
-			e.printStackTrace();
-		}
-		
-		*/
-		
-		/* TESTE LISTAR POR PROXIMIDADE
-		
-		p.setCo_X("31");
-		try {
-			ArrayList<Pontos>lista = pDAO.listarPorProximidade(p);
-			for(Pontos ponto : lista) {
-			System.out.println(ponto);
-			}
-			
-		}catch (SQLException e) {
-			System.out.println("Não foi possivel listar!");
-			e.printStackTrace();
-			// TODO: handle exception
-		}
-		
-		*/
-		
-		//TESTE LISTAR GERAL
-		try {
-			ArrayList<Pontos>lista = pDAO.listar();
-			for(Pontos ponto : lista) {
-			System.out.println(ponto);
-			}
-			
-		}catch (SQLException e) {
-			System.out.println("Não foi possivel listar!");
-			e.printStackTrace();
-			// TODO: handle exception
-		}
-		
 
+		/*
+		 * TESTE ATUALIZAÇÃO
+		 * 
+		 * p.setIdPonto(1L); p.setNome_ponto("Posto"); p.setCo_X("31"); p.setCo_Y("18");
+		 * 
+		 * try{ pDAO.atualizar(p); System.out.println("Ponto atualizado com sucesso!");
+		 * 
+		 * }catch(SQLException e) {
+		 * System.out.println("Ponto não pode ser atualizado!"); e.printStackTrace(); }
+		 * 
+		 */
+
+		
+		//  TESTE LISTAR POR PROXIMIDADE
+		  
+		  p.setCo_X_ref(20L);
+		  p.setCo_Y_ref(10L);
+		  p.setdMax(10L);
+		  try { 
+			  ArrayList<Pontos>lista = pDAO.listarPorProximidade(p);
+		  for(Pontos ponto : lista) { System.out.println(ponto); }
+		  
+		  }catch(SQLException e){
+		System.out.println("Não foi possivel listar!");
+		e.printStackTrace(); 
+		}
+
+		/*
+		 * TESTE LISTAR GERAL try { ArrayList<Pontos> lista = pDAO.listar(); for (Pontos
+		 * ponto : lista) { System.out.println(ponto); }
+		 * 
+		 * } catch (SQLException e) { System.out.println("Não foi possivel listar!");
+		 * e.printStackTrace(); // TODO: handle exception }
+		 */
 	}
 }
--- src/br/com/xyinc/domain/Pontos.java
+++ src/br/com/xyinc/domain/Pontos.java
@@ -4,8 +4,11 @@ public class Pontos {
 	
 	private Long idPonto;
 	private String nome_ponto;
-	private String co_X;
-	private String co_Y;
+	private Long co_X;
+	private Long co_X_ref;
+	private Long co_Y;
+	private Long co_Y_ref;
+	private Long dMax;
 	public Long getIdPonto() {
 		return idPonto;
 	}
@@ -18,20 +21,36 @@ public class Pontos {
 	public void setNome_ponto(String nome_ponto) {
 		this.nome_ponto = nome_ponto;
 	}
-	public String getCo_X() {
+	public Long getCo_X() {
 		return co_X;
 	}
-	public void setCo_X(String co_X) {
+	public void setCo_X(Long co_X) {
 		this.co_X = co_X;
 	}
-	public String getCo_Y() {
+	public Long getCo_Y() {
 		return co_Y;
 	}
-	public void setCo_Y(String co_Y) {
+	public void setCo_Y(Long co_Y) {
 		this.co_Y = co_Y;
 	}
-	
-	
+	public Long getCo_X_ref() {
+		return co_X_ref;
+	}
+	public void setCo_X_ref(Long co_X_ref) {
+		this.co_X_ref = co_X_ref;
+	}
+	public Long getCo_Y_ref() {
+		return co_Y_ref;
+	}
+	public void setCo_Y_ref(Long co_Y_ref) {
+		this.co_Y_ref = co_Y_ref;
+	}
+	public Long getdMax() {
+		return dMax;
+	}
+	public void setdMax(Long dMax) {
+		this.dMax = dMax;
+	}
 	@Override
 	public String toString() {
 		String saida = "Código: " + idPonto + " | Nome do Ponto:  " + nome_ponto + " | Coordenada X:  " + co_X + " | Coordenada Y:  " + co_Y ;
