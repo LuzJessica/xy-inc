37c55648176e833edc1c2c22952812ea50527df3
Jéssica Luz <jessicaluz@gec.inatel.br>
2019-04-02 13:03:56 -0300

    Criação de conexão com o Banco de dados

---
+++ src/br/com/xyinc/factory/ConexaoFactory.java
@@ -0,0 +1,28 @@
+package br.com.xyinc.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoFactory {
+	
+	private static final String USUARIO = "root";
+	private static final String SENHA = "root";
+	private static String URL = "jdbc:mysql://localhost:3306/orica";
+	
+	public static Connection conectar() throws SQLException {
+		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+		return conexao;
+	}
+	
+	public static void main (String[] args) {
+		try {
+			ConexaoFactory.conectar();
+			System.out.println("Conectado com BD com sucesso");
+		}catch(SQLException e) {
+			System.out.println("Não foi possivel conectar ao BD!");
+			e.printStackTrace();
+		}
+	}
+	
+}
